60b9ee4e43fb4b888591f0ed73dac03d66107478
FranciscoTurro <franciscogtrro@outlook.com.ar>
2022-06-15 11:39:22 -0300

    Commit inicial, usuario terminado

---
+++ Controladora/Usuario.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controladora
+{
+    public class Usuario
+    {
+        private static Usuario instancia;
+
+        private Usuario() { }
+
+        public static Usuario obtenerInstancia()
+        {
+            if (instancia == null)
+            {
+                instancia = new Usuario();
+            }
+            return instancia;
+        }
+
+        public void AgregarUsuario(Modelo.Usuario usuario)
+        {
+            Modelo.SingletonContexto.obtenerInstancia().Contexto.Usuarios.Add(usuario);
+            Modelo.SingletonContexto.obtenerInstancia().Contexto.SaveChanges();
+        }
+
+        public void BorrarUsuario(Modelo.Usuario usuario)
+        {
+            Modelo.SingletonContexto.obtenerInstancia().Contexto.Usuarios.Remove(usuario);
+            Modelo.SingletonContexto.obtenerInstancia().Contexto.SaveChanges();
+        }
+
+        public Modelo.Usuario ObtenerUsuario(int codigo)
+        {
+            return Modelo.SingletonContexto.obtenerInstancia().Contexto.Usuarios.Find(codigo);
+        }
+    }
+}
---
+++ Modelo/SingletonContexto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class SingletonContexto
+    {
+        private static SingletonContexto instancia;
+        private static ContextoContainer contexto;
+
+        private SingletonContexto() { }
+
+        public static SingletonContexto obtenerInstancia()
+        {
+            if (instancia == null)
+            {
+                instancia = new SingletonContexto();
+                contexto = new ContextoContainer();
+            }
+            return instancia;
+        }
+        public ContextoContainer Contexto
+        {
+            get { return contexto; }
+        }
+
+    }
+}
