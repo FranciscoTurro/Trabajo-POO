83cc863d5ec034481575321488baea3aa4e6391c
FranciscoTurro <franciscogtrro@outlook.com.ar>
2022-06-16 15:51:48 -0300

    Test

--- Vista/Login.Designer.cs
+++ Vista/Login.Designer.cs
@@ -49,7 +49,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(388, 147);
+            this.button1.Location = new System.Drawing.Point(423, 193);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
--- Vista/Login.cs
+++ Vista/Login.cs
@@ -20,13 +20,13 @@ namespace Vista
         private void button1_Click(object sender, EventArgs e)
         {
             List<Modelo.Usuario> ListaUsuarios = new List<Modelo.Usuario>();
-            ListaUsuarios = Controladora.Usuario.obtenerInstancia().ListaUsuarios();
+            ListaUsuarios = Controladora.Usuario.obtenerInstancia().ListaUsuarios();  //Se crea una lista con todos los usuarios en la base de datos
 
             Modelo.Usuario Neos = new Modelo.Usuario();
             Neos.Nombre = textBox1.Text;
-            Neos.Contraseña = textBox2.Text;
+            Neos.Contraseña = textBox2.Text; 
             
-            Modelo.Usuario x = ListaUsuarios.Find(usuario => usuario.Nombre == Neos.Nombre && usuario.Contraseña == Neos.Contraseña);
+            Modelo.Usuario x = ListaUsuarios.Find(usuario => usuario.Nombre == Neos.Nombre && usuario.Contraseña == Neos.Contraseña); //Se busca en la lista un usuario que tenga el mismo nombre y contraseña que el ingresado
             if (x != null)
             {
                 MessageBox.Show("Usuario existe en la base de datos");
